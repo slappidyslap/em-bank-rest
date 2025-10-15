7be5305baa043e220702cfb9a260061b67664f9a
Eldiyar <musabaeveldiar@gmail.com>
2025-10-15 15:30:18 +0600

    init

---
+++ src/main/java/kg/musabaev/em_bank_rest/EmBankRestApplication.java
@@ -0,0 +1,13 @@
+package kg.musabaev.em_bank_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmBankRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmBankRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/musabaev/em_bank_rest/EmBankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.musabaev.em_bank_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmBankRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
